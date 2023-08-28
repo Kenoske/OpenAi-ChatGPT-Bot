55a685459108486ef8a4b6954745ec893378620c
Kenoske <131994720+Kenoske@users.noreply.github.com>
2023-08-28 14:31:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,8 +27,8 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-global.AI_PASSWORD = 'im'
-global.SESSION_ID = ''
+global.AI_PASSWORD = 'ai password'
+global.SESSION_ID = 'elisa_md@;;;gFUwXSYQ#FL2scR3cLkogqqL1NcVZqC0TYVMKJ8Dd8NFBYsYr8ns'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
